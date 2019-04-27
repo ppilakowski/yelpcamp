f719d12683fa1da18d85b2524719b25be458bf4b
Pilakowski <ppilakowski@psi.de>
2019-04-27 10:41:31 +0200

    Changes in seeds.js, added ./modals/comment.js

--- seeds.js
+++ seeds.js
@@ -1,59 +1,58 @@
-const mongoose = require("mongoose");
+var mongoose = require("mongoose");
 var Campground = require("./models/campground");
+var Comment   = require("./models/comment");
 
 var data = [
     {
-        name: "Pierwszy",
-        image: "https://dascompany.com/render/3-6/arrows/61/opt_S3V6-A055K0A4.png",
-        description: "Opis pierwszego campgrounda"
+        name: "Cloud's Rest", 
+        image: "https://farm4.staticflickr.com/3795/10131087094_c1c0a1c859.jpg",
+        description: "blah blah blah"
     },
     {
-        name: "Drugi",
-        image: "https://dascompany.com/uploads/tent/boxes/gates/namiot-magazynowy-otwieranie-3-1.jpg",
-        description: "Opis drugiego campgrounda"
+        name: "Desert Mesa", 
+        image: "https://farm4.staticflickr.com/3859/15123592300_6eecab209b.jpg",
+        description: "blah blah blah"
     },
     {
-        name: "Trzeci",
-        image: "https://dascompany.com/uploads/tent/boxes/gates/namiot-magazynowy-otwieranie-3-2.jpg",
-        description: "Opis trzeciego campgrounda"
+        name: "Canyon Floor", 
+        image: "https://farm1.staticflickr.com/189/493046463_841a18169e.jpg",
+        description: "blah blah blah"
     }
 ]
 
-
-// Remove all campgrounds
 function seedDB(){
-    Campground.remove({}, function(err){
+   //Remove all campgrounds
+   Campground.remove({}, function(err){
         if(err){
-            console.log(err)
-        } else {
-            console.log("removed campgrounds");
-            // add a few campgrounds
-            data.forEach(function(seed){
-                Campground.create(seed, function(err, campground){
-                    if(err){
-                        console.log(err)
-                    } else {
-                        console.log("Campground created")
-                        //create a comment
-                        Comment.create(
-                            {
-                                text: "Jest super",
-                                author: "Homer"
-                            }, function(err, comment){
-                                if(err){
-                                    console.log(err)
-                                } else {
-                                    campground.comments.push(comment);
-                                    campground.save();
-                                    console.log("comment added")
-                                }
-                            }
-                        )
-                    }
-                })
-            })
+            console.log(err);
         }
-    })
+        console.log("removed campgrounds!");
+         //add a few campgrounds
+        data.forEach(function(seed){
+            Campground.create(seed, function(err, campground){
+                if(err){
+                    console.log(err)
+                } else {
+                    console.log("added a campground");
+                    //create a comment
+                    Comment.create(
+                        {
+                            text: "This place is great, but I wish there was internet",
+                            author: "Homer"
+                        }, function(err, comment){
+                            if(err){
+                                console.log(err);
+                            } else {
+                                campground.comments.push(comment);
+                                campground.save();
+                                console.log("Created new comment");
+                            }
+                        });
+                }
+            });
+        });
+    }); 
+    //add a few comments
 }
 
 module.exports = seedDB;
