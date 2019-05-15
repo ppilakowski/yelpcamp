6751d15b1bebc35790c1a34c3589fea713ed4885
Pilakowski <ppilakowski@psi.de>
2019-05-15 20:55:14 +0200

    Added page and routes for adding a commit

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const ejs        = require("ejs");
 const bodyParser = require('body-parser')
 const mongoose   = require('mongoose');
 var Campground   = require("./models/campground");
+var Comment      = require("./models/comment");
 var seedDB       = require("./seeds")
 
 
@@ -29,7 +30,7 @@ app.get("/campgrounds", function(req, res){
             console.log("Mamy babola!!!");
             console.log(err)
         } else {
-            res.render("index", {campgrounds: allCampgrounds});
+            res.render("campgrounds/index", {campgrounds: allCampgrounds});
         }
     })
 });
@@ -55,7 +56,7 @@ app.post("/campgrounds", function(req, res){
 });
 
 app.get("/campgrounds/new", function(req,res){
-    res.render("new");
+    res.render("campgrounds/new");
 })
 
 
@@ -68,9 +69,49 @@ app.get("/campgrounds/:id", function(req, res){
         } else {
             console.log(foundCampground)
             //render show template with that campground
-            res.render("show", {campground: foundCampground});
+            res.render("campgrounds/show", {campground: foundCampground});
         }
     });
+})
+
+
+// =============================
+// COMMENTS ROUTES
+// =============================
+
+app.get("/campgrounds/:id/comments/new", function(req, res){
+    //find campground by id
+    Campground.findById(req.params.id, function(err, campground){
+        if(err){
+            console.log(err)
+        } else {
+            res.render("comments/new", {campground: campground})
+        }
+    })
+})
+
+app.post("/campgrounds/:id/comments/", function(req, res){
+    //lookup campground using id
+    Campground.findById(req.params.id, function(err, campground){
+        if(err){
+            console.log(err);
+            res.redirect("/campgrounds");
+        } else {
+            //create new comment
+            Comment.create(req.body.comment, function(err,comment){
+                if(err){
+                    console.log(err)
+                } else {
+                    campground.comments.push(comment);
+                    campground.save();
+                    res.redirect("/campgrounds/" + campground._id);
+                }
+            })
+            //connect new comment to campground
+            //redirect to show page of campground
+        }
+    })
+
 })
 
 
